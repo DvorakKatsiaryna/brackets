62fc0ecb8fb1cfce5490fe8450dd971df59d06c6
DvorakKatsiaryna <dvorak.katsiaryna@gmail.com>
2021-02-25 23:01:40 +0400

    fix: fix bugs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function check(str, bracketsConfig) {
   let newStr = str;
-  for (i = 0; i < str.length / 2; i++) {
-    for (j = 0; j < bracketsConfig.length; j++) {
+  for (let i = 0; i < str.length / 2; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
       newStr = newStr.replace(bracketsConfig[j][0] + bracketsConfig[j][1], '');
     }
   }
